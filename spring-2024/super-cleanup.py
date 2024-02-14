124d4927876f2298c0324c26d99d0a49f8359c71
MDN <mark.denardo@gmail.com>
2024-02-14 12:54:28 -0500

    Update super-cleanup.py

--- spring-2024/super-cleanup.py
+++ spring-2024/super-cleanup.py
@@ -1,16 +1,15 @@
-from hub import port firmopmorstpirkuenliomopport ForceSensor, Motor import motor
+from hub import port
+import runloop
+import motor
 import force_sensor
-force_sensor = ForceSensor('E')
-grabber_motor = Motor('A')
+
 async def main():
     while True:
-while True:
-# Wait for the force sensor to be pressed.
-await runloop.until(lambda: force_sensor.pressed(port.E)) force_sensor.wait_until_pressed()
-motor.run(port.A, -750) grabber_motor.set_stall_detection(False)
-grabber_motor.start(-75)
-# Wait for the force sensor to released.
-force_sensor.wait_until_released()
-await runloop.until(lambda: not force_sensor.pressed(port.E)) grabber_motor.set_stall_detection(True)
-motor.run(port.A, 750) grabber_motor.start(75)
+        # Wait for the force sensor to be pressed
+        await runloop.until(lambda: force_sensor.pressed(port.E))
+        motor.run(port.A,-750)
+        # Wait for the force sensor to be released
+        await runloop.until(lambda: not force_sensor.pressed(port.E))
+        motor.run(port.A, 750)
+
 runloop.run(main())
